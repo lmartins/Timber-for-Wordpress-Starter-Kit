ded6e76997334e981ba2221c0d8358a29203826c
Luis Martins <luis.martins@multiweb.pt>
2014-12-20 19:50:28 +0000

    Theme Hook Alliance implemented

--- wip-child/functions.php
+++ wip-child/functions.php
@@ -3,3 +3,10 @@
 require_once( get_template_directory() . '/mdf/init.php' );
 
 require_once( CHILD_DIR . '/inc/sidebars.php' );
+
+
+// add_action('tha_entry_top','teste');
+// function teste()
+// {
+//     echo 'teste';
+// }
