3c354b6bedbdab3d361974480c02971cd8b12932
Luis Martins <luis.martins@multiweb.pt>
2014-12-21 22:59:56 +0000

    Parent theme update

---
+++ wip-parent-theme/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+if (!class_exists('Timber')){
+	add_action( 'admin_notices', function(){
+		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . admin_url('plugins.php#timber') . '">' . admin_url('plugins.php') . '</a></p></div>';
+	});
+	return;
+}
+
+
+require_once( PARENT_DIR . '/lib/tha-theme-hooks.php' );
+
+require_once( PARENT_DIR . '/mdf/archives.php' );
+require_once( PARENT_DIR . '/mdf/pages.php' );
+require_once( PARENT_DIR . '/mdf/single.php' );
+require_once( PARENT_DIR . '/mdf/security.php' );
+
+require_once( PARENT_DIR . '/mdf/admin/editor.php' );
+require_once( PARENT_DIR . '/mdf/admin/lists.php' );
+
